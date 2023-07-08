9e72edee62431068abd06b13893f23775aa56cc7
trkinvincible <trkinvincible85@yahoo.com>
2023-07-08 15:39:23 +0100

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,11 +1,7 @@
 #include <iostream>
 #include <memory>
 #include "./command.h"
-#include "./sfine_example.h"
-#include "./static_keys_map.h"
-#include "./stdfunction_impl.h"
-#include "./stdUnique_Impl.h"
-#include "./ways_to_print_container.h"
+#include "./hft_prob_1.h"
 
 int main(int argc, char *argv[])
 {
@@ -16,34 +12,10 @@ int main(int argc, char *argv[])
 
     case 1:
     {
-        obj.reset(new sfine_example());
-    }
-        break;
-    case 2:
-    {
-        obj.reset(new static_keys_map());
-    }
-        break;
-
-    case 3:
-    {
-        obj.reset(new stdfunction_impl());
-    }
-        break;
-
-    case 4:
-    {
-        obj.reset(new stdUnique_Impl());
-    }
-        break;
-    case 5:
-    {
-        print_sequence(std::integer_sequence<unsigned, 9, 2, 5, 1, 9, 1, 6>{});
-        printer(1, 2, 3, "abc");
+        obj.reset(new hft_prob_1());
     }
         break;
     }
-
     if (obj)
         obj->execute();
 }

b5321333cc55c20acecb795bb25e30db978be0fe
trkinvincible <trkinvincible85@yahoo.com>
2023-07-09 11:22:59 +0100

    more examples
    # Conflicts:
    #	CMakeLists.txt
    #	hft_prob_1.h
    #	main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
 #include <memory>
 #include "./command.h"
-#include "./sfine_example.h"
-#include "./static_keys_map.h"
-#include "./stdfunction_impl.h"
-#include "./stdUnique_Impl.h"
-#include "./ways_to_print_container.h"
+#include "./hft_prob_1.h"
+#include "./hft_prob_2.h"
+#include "./hft_prob_3.h"
 
 int main(int argc, char *argv[])
 {
@@ -16,30 +14,18 @@ int main(int argc, char *argv[])
 
     case 1:
     {
-        obj.reset(new sfine_example());
+        obj.reset(new hft_prob_1());
     }
         break;
     case 2:
     {
-        obj.reset(new static_keys_map());
+        obj.reset(new hft_prob_2());
     }
         break;
 
     case 3:
     {
-        obj.reset(new stdfunction_impl());
-    }
-        break;
-
-    case 4:
-    {
-        obj.reset(new stdUnique_Impl());
-    }
-        break;
-    case 5:
-    {
-        print_sequence(std::integer_sequence<unsigned, 9, 2, 5, 1, 9, 1, 6>{});
-        printer(1, 2, 3, "abc");
+        obj.reset(new hft_prob_3());
     }
         break;
     }

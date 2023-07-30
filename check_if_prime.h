9dedec6295f17cd4598202b21501ebc9fe7dfbb0
trkinvincible <trkinvincible85@yahoo.com>
2023-07-30 13:02:25 +0100

    removed template sopicialization for is prime

--- check_if_prime.h
+++ check_if_prime.h
@@ -1,12 +1,12 @@
-#ifndef CHECK_IF_PRIME_H
-#define CHECK_IF_PRIME_H
+#pragma once
 
 #include <iostream>
 
+#if 0
 template<std::size_t T, std::size_t D = 2>
 struct IsPrime
 {
-    static constexpr bool value = (T == D ? true : (T%D == 0 ? false : IsPrime<T, D + 1>::value));
+    static constexpr bool value = T == D ? true : (T%D == 0 ? false : IsPrime<T, D + 1>::value);
 };
 
 template<std::size_t T>
@@ -14,11 +14,28 @@ struct IsPrime<T, T>
 {
     static constexpr bool value = true;
 };
+#else
+template<std::size_t T, std::size_t D = 2>
+struct IsPrime
+{
+    static constexpr bool value = []() {
+      if constexpr (T == D)
+        return true;
+      else if constexpr (T%D == 0)
+        return false;
+      else
+        return IsPrime<T, D + 1>::value;
+    }();
+};
+#endif
 
+template<std::size_t T>
+struct IsPrime<2, T>
+{
+    static constexpr bool value = true;
+};
 template<std::size_t T>
 struct IsPrime<1, T>
 {
     static constexpr bool value = false;
 };
-
-#endif // CHECK_IF_PRIME_H
